74a0883c2d65395ff5412a0213b033431e684a34
sadinity <amydyerson@gmail.com>
2025-12-29 23:28:10 +0100

    Initial commit

---
+++ src/main/java/net/sadinity/wizardry/Wizardry.java
@@ -0,0 +1,17 @@
+package net.sadinity.wizardry;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Wizardry implements ModInitializer {
+	public static final String MOD_ID = "wizardry";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/sadinity/wizardry/WizardryClient.java
@@ -0,0 +1,10 @@
+package net.sadinity.wizardry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WizardryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
