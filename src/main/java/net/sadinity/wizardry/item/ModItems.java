7ab422b091ab0d3427e45ee0b2c2eca0c5f0195a
sadinity <amydyerson@gmail.com>
2026-01-01 00:48:50 +0100

    teehee update :P

--- src/main/java/net/sadinity/wizardry/item/ModItems.java
+++ src/main/java/net/sadinity/wizardry/item/ModItems.java
@@ -6,11 +6,12 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.sadinity.wizardry.Wizardry;
 
 public class ModItems {
     public static final Item REGULAR_WAND = registerItem("regular_wand", new Item(new Item.Settings()));
-    public static final Item MAGIC_ESSENCE = registerItem("magic_essence", new Item(new Item.Settings()));
+    public static final Item MAGIC_ESSENCE = registerItem("magic_essence", new Item(new Item.Settings().rarity(Rarity.UNCOMMON)));
     public static final Item MAGIC_PARTICLES = registerItem("magic_particles", new Item(new Item.Settings()));
     public static final Item ELDER_DOOR = registerItem("elder_door", new Item(new Item.Settings()));
 
