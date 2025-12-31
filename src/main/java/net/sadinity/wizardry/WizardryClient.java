1d3f5121227411e13a8ab76937c86ebf53befd3a
sadinity <amydyerson@gmail.com>
2025-12-31 18:51:48 +0100

    teehee update :P

--- src/main/java/net/sadinity/wizardry/WizardryClient.java
+++ src/main/java/net/sadinity/wizardry/WizardryClient.java
@@ -1,10 +1,14 @@
 package net.sadinity.wizardry;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.sadinity.wizardry.block.ModBlocks;
 
 public class WizardryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ELDER_TRAPDOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ELDER_DOOR, RenderLayer.getCutout());
     }
 }
